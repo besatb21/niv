f79800977fe7212a0413295179fe0318eea36ad2
besa <b.jacaj1@gmail.com>
2024-09-14 00:58:04 +0200

    Add upload audio and change frequency using ffmpeg

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Encoder from '../components/Encoder.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Encoder />
   </main>
 </template>
